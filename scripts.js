c346da559447014e94704f0991a309f0fc6bb82b
Jevon Lyons <jevonlyons10@gmail.com>
2025-11-06 21:08:30 -0600

    Final project update: added JS and images

--- scripts.js
+++ scripts.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const subscribeButton = document.getElementById("subscribe");
   const emailInput = document.getElementById("email");
+  const subscribeButton = document.getElementById("subscribe");
   const messageDiv = document.querySelector(".message");
 
-  subscribeButton.addEventListener("click", function (event) {
-    event.preventDefault(); // Stop refresh
-
+  subscribeButton.addEventListener("click", function () {
     const email = emailInput.value.trim();
 
-    if (email !== "") {
-      messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
-      messageDiv.style.color = "white";
-    } else {
+    if (email === "") {
       messageDiv.textContent = "Please enter a valid email address.";
       messageDiv.style.color = "yellow";
+    } else {
+      messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+      messageDiv.style.color = "white";
     }
   });
 });
