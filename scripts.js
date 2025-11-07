681316ddc56d5798743f27fc07878a7541c0dcf9
Jevon Lyons <jevonlyons10@gmail.com>
2025-11-06 20:57:03 -0600

    Fixed newsletter JavaScript functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function () {
   const subscribeButton = document.getElementById("subscribe");
   const emailInput = document.getElementById("email");
   const messageDiv = document.querySelector(".message");
 
-  subscribeButton.addEventListener("click", function(event) {
-    event.preventDefault();
+  subscribeButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Stop refresh
 
     const email = emailInput.value.trim();
 
@@ -16,4 +16,4 @@ window.onload = function() {
       messageDiv.style.color = "yellow";
     }
   });
-};
+});
